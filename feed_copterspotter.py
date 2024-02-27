49ec5e90a2e7c0284c288c78c6d47e24319dcf21
Daniel H Brown <daniel.h.brown@gmail.com>
2024-02-27 09:00:02 -0500

    used global recent_flights

--- feed_copterspotter.py
+++ feed_copterspotter.py
@@ -164,13 +164,13 @@ def mongo_https_insert(mydict):
     return response.status_code
 
 
-def dump_recents(signum, flights):
+def dump_recents(signum):
     """Dump recents if we get a sigusr1"""
     signame = signal.Signals(signum).name
     logger.info(f"Signal handler dump_recents called with signal {signame} ({signum})")
 
-    for hex_icao in sorted(flights):
-        logger.info("hex_icao: %s flight: %s", hex_icao, flights[hex_icao])
+    for hex_icao in sorted(recent_flights):
+        logger.info("hex_icao: %s flight: %s", hex_icao, recent_flights[hex_icao])
 
 
 def update_helidb():
@@ -180,7 +180,7 @@ def update_helidb():
 
     # Set the signal handler to dump recents
 
-    signal.signal(signal.SIGUSR1, dump_recents(recent_flights))
+    signal.signal(signal.SIGUSR1, dump_recents())
 
     try:
         #        with open("/run/" + AIRPLANES_FOLDER + "/aircraft.json") as json_file:
